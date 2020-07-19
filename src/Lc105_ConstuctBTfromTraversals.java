59bd69d7d889457a070a0f052357807e86f007ec
Nesar Ahmed <nesar13@hotmail.com>
2020-07-19 14:48:01 -0400

    TreeNode definition added

--- src/Lc105_ConstuctBTfromTraversals.java
+++ src/Lc105_ConstuctBTfromTraversals.java
@@ -1,2 +1,34 @@
 public class Lc105_ConstuctBTfromTraversals {
+    static class TreeNode {
+        int val;
+        Lc107_BST_TreeTraversal.TreeNode left;
+        Lc107_BST_TreeTraversal.TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+
+        TreeNode(int val, Lc107_BST_TreeTraversal.TreeNode left, Lc107_BST_TreeTraversal.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+
+
+        }
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+        public static void main(String[] args) {
+
+        }
+    }
 }
