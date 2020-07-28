fc9d47241e1b691f08ed1d1936ccd8cdd47369cf
Nesar Ahmed <nesar13@hotmail.com>
2020-07-28 14:23:01 -0400

    created solution using hashmap

--- src/Lc106_ConstructBTfromPostandInorder.java
+++ src/Lc106_ConstructBTfromPostandInorder.java
@@ -29,40 +29,38 @@ public class Lc106_ConstructBTfromPostandInorder {
                     '}';
         }}
 
-    HashMap<Integer, Integer> i_map=new HashMap<>();
-    int postindex;
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
+    static HashMap<Integer, Integer> i_map=new HashMap<>();
+    static int postindex;
+    public static TreeNode buildTree(int[] inorder, int[] postorder) {
 
         for (int i = 0; i < inorder.length; i++) {
 
             //caching the inorder
             i_map.put(inorder[i], i);
-
-
         }
-
+        postindex=postorder.length-1;
         return recursion(inorder, postorder, 0, inorder.length-1);
 
     }
 
-    private TreeNode recursion(int[] inorder, int[] postorder, int start, int end) {
+    private static TreeNode recursion(int[] inorder, int[] postorder, int start, int end) {
         if (start>end)
             return null;
-        TreeNode root=new TreeNode(postindex++);
+        TreeNode root=new TreeNode(postorder[postindex--]);
 
         if(root==null)
             return null;
 
         if(start==end)
-            return null;
+            return root;
 
         int index=i_map.get(root.val);
 
-
+        //note when using postorder, do right side first
+        root.right=recursion(inorder, postorder, index+1, end);
         root.left=recursion(inorder, postorder, start, index-1);
-        root.right=recursion(inorder, postorder, index-1, end );
 
-
+        return root;
 
     }
 
@@ -71,7 +69,7 @@ public class Lc106_ConstructBTfromPostandInorder {
         int[] postorder={9,15,7,20,3};
         int[] inorder={9,3,15,20,7};
 
-        /*System.out.println(buildTree(preorder, inorder));*/
+        System.out.println(buildTree(inorder, postorder));
 
     }
 
