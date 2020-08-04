a375543c14bd5fa5042b4e2e3f6e72b9756ffde3
Nesar Ahmed <nesar13@hotmail.com>
2020-08-04 15:32:10 -0400

    solved lc206 reverse linkedList

--- src/Lc206_ReverseLinkedList.java
+++ src/Lc206_ReverseLinkedList.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 
 public class Lc206_ReverseLinkedList {
 
@@ -9,34 +10,75 @@ public class Lc206_ReverseLinkedList {
      ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
     public static ListNode reverseList(ListNode head) {
-    ListNode dummy=new ListNode(0);
+    ListNode dummy=null;
     HashMap<Integer, Integer> map=new HashMap<>();
 
     int count=0;
-    head=dummy;
+   /* dummy.next=head;*/
+  /*  ListNode res=dummy.next*/;
 
-    while(head!=null) {
+    while(head !=null) {
+        map.put(count++, head.val);
         head=head.next;
-        count++;
-        map.put(count, head.val);
+
+
 
     }
+ListNode curr=new ListNode(0);
+    dummy=curr;
+    while(count > 0) {
 
-    while(count >= 0) {
+        curr.next= new ListNode(map.get(count-1));
 
-        dummy=new ListNode(map.get(count));
+      curr=curr.next;
 
-        dummy=dummy.next;
+
 
         count--;
     }
 
-    return dummy;
+    return dummy.next;
+    }
+
+
+    public static ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
     }
 
     public static void main(String[] args) {
+int count=0;
+HashMap<Integer, Integer> map=new HashMap<>();
+        ListNode head=new ListNode(1);
+        head.next=new ListNode(2);
+        head.next.next=new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        head.next.next.next.next=new ListNode(5);
+       /* for (int i = 0; i < 6; i++) {
+            head.next=new ListNode(i);
+            head=head.next;
+        }*/
+
+
+       /* System.out.println(head);*/
+        System.out.println(reverseList2(head));
 
     }
 }
