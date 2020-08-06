fe88507c0279d6cb0cc3514ea8652d8b0714ea12
Nesar Ahmed <nesar13@hotmail.com>
2020-08-06 14:37:07 -0400

    added comments to this

--- src/Lc283_MoveZeros.java
+++ src/Lc283_MoveZeros.java
@@ -10,13 +10,15 @@ public class Lc283_MoveZeros {
         for (int i = 0; i < nums.length; i++) {
 
             if (nums[i] != 0) {
+                //Swapping all the non-zero values to the beginning of the array
                 nums[start++] = nums[i];
             } else
+                //otherwise we want to count the zeros for later
                 countZeros++;
         }
 
         while (countZeros != 0) {
-
+            //adding the zeros at the end of the list
             nums[end - countZeros] = 0;
 
             countZeros--;
