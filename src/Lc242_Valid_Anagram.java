1203d2b8759b7526a5918aa9ed738e8ad17289bc
Nesar Ahmed <nesar13@hotmail.com>
2020-08-07 13:13:58 -0400

    added Lc242_Valid_Anagram.java

---
+++ src/Lc242_Valid_Anagram.java
@@ -0,0 +1,24 @@
+public class Lc242_Valid_Anagram {
+
+    public boolean isAnagram(String s, String t) {
+        boolean[] arr=new boolean[26];
+
+
+        for (int i = 0; i < t.length(); i++) {
+
+
+
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+
+    }
+
+}
