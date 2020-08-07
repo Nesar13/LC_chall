bff132c0a8ebfd33a5e72d99ece35e3ce60932e9
Nesar Ahmed <nesar13@hotmail.com>
2020-08-07 11:29:20 -0400

    added solution to Lc122_BestTimetoBuyandSellStocks.java

--- src/Lc122_BestTimetoBuyandSellStocks.java
+++ src/Lc122_BestTimetoBuyandSellStocks.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+
 public class Lc122_BestTimetoBuyandSellStocks {
+
+
+    public static int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+
+        int[] arr={1, 7, 2, 3, 6, 7, 6, 7};
+
+        System.out.println(maxProfit(arr));
+    }
 }
