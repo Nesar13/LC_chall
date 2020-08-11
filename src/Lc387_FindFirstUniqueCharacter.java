abd55a69ff2d9ea1bbc7268e24a50e25162de50e
Nesar Ahmed <nesar13@hotmail.com>
2020-08-11 14:47:13 -0400

    Adding Lc387_FindFirstUniqueCharacter.java

--- src/Lc387_FindFirstUniqueCharacter.java
+++ src/Lc387_FindFirstUniqueCharacter.java
@@ -1,2 +1,29 @@
 public class Lc387_FindFirstUniqueCharacter {
+    public static int firstUniqChar(String s) {
+        int [] arr= new int [26];
+
+
+        for (int i =0; i<s.length(); i++){
+
+            arr[s.charAt(i)-'a']++;
+
+        }
+
+        for (int i =0; i<s.length(); i++){
+
+            if ( arr[s.charAt(i)-'a'] < 2)
+                return i;
+
+        }
+
+        return -1;
+
+    }
+    public static void main(String[] args) {
+
+        String s="leetcode";
+
+        System.out.println(firstUniqChar(s));
+
+    }
 }
