64b9761f3e77e1d0c4789ca067c12dfca48adbac
Nesar Ahmed <nesar13@hotmail.com>
2020-08-25 15:38:12 -0400

    Added solution to Lc350_IntersectionOfArray.java

---
+++ src/Lc350_IntersectionOfArray.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Lc350_IntersectionOfArray {
+
+
+    //fastes solution
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> list= new ArrayList<>();
+        Map<Integer,Integer> map= new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i],map.getOrDefault(nums1[i],1)+1);
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i])){
+                int new_val=map.get(nums2[i])-1;
+                if(new_val==0){
+                    map.remove(nums2[i]);
+                }else{
+                    map.put(nums2[i],new_val);
+                    list.add(nums2[i]);
+                }
+            }
+        }
+        int[] arr= new int[list.size()];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
+    }
+
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int k = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0)+1);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                nums1[k] = nums2[i];
+                k++;
+            }
+            map.put(nums2[i], map.getOrDefault(nums2[i], 0)-1);
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int [] nums1= {1,2,2,4,5};
+        int [] nums2= {5,3,2,4,7};
+
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+
+    }
+}
