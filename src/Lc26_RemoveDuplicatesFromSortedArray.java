2e1aa632dd6a649b4e26f0a54239c8b275661c11
Nesar Ahmed <nesar13@hotmail.com>
2020-09-10 16:43:45 -0400

    Added two pointer solution

---
+++ src/Lc26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Lc26_RemoveDuplicatesFromSortedArray {
+
+
+    // Uses two pointers, one slow and one fast
+    // 100% fast
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+    public static void main(String[] args) {
+
+        int[] arr= {0,0,1,1,1,2,2,3};
+        System.out.println(removeDuplicates(arr));
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
--- src/Lc53_MaxSubarray.java
+++ src/Lc53_MaxSubarray.java
@@ -5,9 +5,17 @@ public class Lc53_MaxSubarray {
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
 
-        for (int i = 1; i < nums.length; i++)
+        for (int i = 1; i < nums.length; i++) {
+            int x_dp=dp[i-1];
+            int y_nums=nums[i];
+            int sum=x_dp+y_nums;
+            System.out.println("dp[i-1]: "+ x_dp);
+            System.out.println("nums[i]: "+ y_nums);
+            //store only the highest number
+            // if the sum is smaller than the current number then
+            // you should not store it
             dp[i] = Math.max(nums[i] + dp[i - 1], nums[i]);
-
+        }
         int max = dp[0];
 
         for (int i = 1; i < nums.length; i++)
