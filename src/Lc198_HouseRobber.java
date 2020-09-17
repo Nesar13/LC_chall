4e35de7f44744b51b726be1077ae301de867eac9
Nesar Ahmed <nesar13@hotmail.com>
2020-09-17 17:01:32 -0400

    added solutions to Lc198_HouseRobber.java

---
+++ src/Lc198_HouseRobber.java
@@ -0,0 +1,35 @@
+public class Lc198_HouseRobber {
+    public static int rob(int[] nums) {
+
+        if (nums.length==0) return 0;
+        if (nums.length==1) return nums[0];
+
+        // create a new array to store the sum
+        int[] dp=new int[nums.length];
+        // initialize the first and second value of new array
+        dp[0]=nums[0];
+        //store the max value between the first and second index
+        dp[1]=Math.max(nums[1],nums[0]);
+        int currMax=Integer.MIN_VALUE;
+
+        for (int i = 2; i < nums.length ; i++) {
+
+            // since we are only interested in every other house
+            // we get the max of the sum of every other house
+            // and the previous value in the new array
+            dp[i]=Math.max(nums[i] +dp[i-2], dp[i-1]);
+            
+        }
+
+        return dp[dp.length-1];
+    }
+
+    
+    public static void main(String[] args) {
+        int[] nums = {2,7,9,3,1};
+        int[] nums2 = {4,2,3,4};
+        int[] nums3 = {2,1,1,2};
+
+        System.out.println(rob(nums3));
+    }
+}
