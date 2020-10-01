1a5b750f39fdf9833f58462969ba32d3355ac31a
Nesar Ahmed <nesar13@hotmail.com>
2020-10-01 14:51:02 -0400

    Added solutions to Lc46_Permutations_M.java

--- src/Lc46_Permutations_M.java
+++ src/Lc46_Permutations_M.java
@@ -28,12 +28,41 @@ public class Lc46_Permutations_M {
         }
     }
 
+    public static List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> mainList=new ArrayList<>();
+        helper2(nums, mainList, new ArrayList<Integer>());
+
+        return mainList;
+
+    }
+
+    public static void helper2 (int [] nums,   List<List<Integer>> mainList, List<Integer> tempList) {
+        if (tempList.size()==nums.length) {
+            mainList.add(new ArrayList<>(tempList));
+        }
+
+        else {
+            for (int i = 0; i <nums.length ; i++) {
+                if(tempList.contains(nums[i])) continue;
+                tempList.add(nums[i]);
+
+                helper2(nums, mainList, tempList);
+
+                tempList.remove(tempList.size()-1);
+            }
+
+
+        }
+
+
+    }
+
 
     public static void main(String[] args) {
 
 
         int[] nums= {1,2,3,4};
-        System.out.println(permute(nums));
+        System.out.println(permute2(nums));
 
 
     }
