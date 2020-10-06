7dc09d4f9309899122fc51a8f3298c57efe33f72
Nesar Ahmed <nesar13@hotmail.com>
2020-10-06 16:29:49 -0400

    Lc78_Subsets_M solved

--- src/Lc78_Subsets_M.java
+++ src/Lc78_Subsets_M.java
@@ -1,29 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Lc78_Subsets_M {
 
 
-    public List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        helper(nums, new ArrayList<Integer>(), res);
+        Arrays.sort(nums);
+        helper(nums, new ArrayList<Integer>(), res, 0);
 
         return res;
 
     }
 
-    private void helper(int[] nums, ArrayList<Integer> tempList, List<List<Integer>> res) {
+    private static void helper(int[] nums, ArrayList<Integer> tempList, List<List<Integer>> res, int start) {
+        // note that you have to create a new ArrayList
+        res.add(new ArrayList<>(tempList));
 
-        res.add(tempList);
-
-        for (int i = 0; i <; i++) {
+        for (int i = start; i <nums.length ; i++) {
+            tempList.add(nums[i]);
+            // note that i will be incremented by 1 each time so there is no duplicates
+            helper(nums, tempList, res, i+1);
+            tempList.remove(tempList.size()-1);
 
         }
+
+        
 
     }
 
 
     public static void main(String[] args) {
+
+        int[] nums= {1,2,3};
+
+        System.out.println(subsets(nums));
 
 
 
