2af0e6649b21b0ad3d8c2885ef0046312cb97702
Nesar Ahmed <nesar13@hotmail.com>
2020-10-29 17:35:26 -0400

    added solution for Lc49_GroupAnagrams

---
+++ src/Lc49_GroupAnagrams.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Lc49_GroupAnagrams {
+
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s); // add to the key, another word/string
+        }
+        return new ArrayList(ans.values()); // returning all the values
+    }
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        System.out.println(groupAnagrams(strs));
+    }
+}
