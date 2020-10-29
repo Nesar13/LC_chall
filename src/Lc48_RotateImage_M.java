19116d4ce24f3358ee8e028596bec0bdfd274f46
Nesar Ahmed <nesar13@hotmail.com>
2020-10-29 16:01:59 -0400

    added solution for Lc48_RotateImage_M

--- src/Lc48_RotateImage_M.java
+++ src/Lc48_RotateImage_M.java
@@ -1,15 +1,29 @@
 import java.util.Arrays;
 
 public class Lc48_RotateImage_M {
-public int[][] rotate(int[][] matrix) {
-    for (int [] rows : matrix){
 
-        for(int cols: rows) {
-            return matrix;
+
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
         }
     }
-    return matrix;
-}
     public static void main(String[] args) {
 //        int [][] arr = {{1,2,3},{4,5,6}, {7,8,9}}; // rows
         int [][] arrs =
@@ -17,12 +31,16 @@ public int[][] rotate(int[][] matrix) {
                 {4,5,6},
                 {7,8,9}};
 
-        for (int i = 0; i < arrs.length; i++) {
-            int[] arr = arrs[i];
+//        for (int i = 0; i < arrs.length; i++) {
+//            int[] arr = arrs[i];
+//
+//            System.out.println("first: " + Arrays.toString(arr));
+//            Arrays.stream(arr).mapToObj(num -> "second: " + num).forEach(System.out::println);
+//        }
+//        System.out.println(Arrays.deepToString(arrs));
 
-            System.out.println("first: " + Arrays.toString(arr));
-            Arrays.stream(arr).mapToObj(num -> "second: " + num).forEach(System.out::println);
-        }
+        rotate(arrs);
+
         System.out.println(Arrays.deepToString(arrs));
     }
 }
