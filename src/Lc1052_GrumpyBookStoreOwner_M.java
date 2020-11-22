82195c20e4564c8366d49a4312826f4e701fc3de
Nesar Ahmed <nesar13@hotmail.com>
2020-11-22 00:52:17 -0500

    Added solutions to Lc1052_GrumpyBookStoreOwner_M

--- src/Lc1052_GrumpyBookStoreOwner_M.java
+++ src/Lc1052_GrumpyBookStoreOwner_M.java
@@ -1,7 +1,7 @@
 public class Lc1052_GrumpyBookStoreOwner_M {
 
 
-
+//
     public static int maxSatisfied2(int[] customers, int[] grumpy, int X) {
         int satisfied = 0, maxMakeSatisfied = 0;
         for (int i = 0, winOfMakeSatisfied = 0; i < grumpy.length; ++i) {
