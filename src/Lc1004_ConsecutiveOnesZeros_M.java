482a7025b2ba33c496bc3f34cd19243583c07d14
Nesar Ahmed <nesar13@hotmail.com>
2020-11-30 16:06:28 -0500

    check

--- src/Lc1004_ConsecutiveOnesZeros_M.java
+++ src/Lc1004_ConsecutiveOnesZeros_M.java
@@ -12,7 +12,7 @@ public class Lc1004_ConsecutiveOnesZeros_M {
     public static void main(String[] args) {
         int[] arr= {1,1,1,1,0,0,0,0};
 
-
+//
         System.out.println(longestOnes(arr, 2));
     }
 }
