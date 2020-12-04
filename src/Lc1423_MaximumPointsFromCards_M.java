e0f345ec06d178badbf67607203e07bdab6ff33f
Nesar Ahmed <nesar13@hotmail.com>
2020-12-04 17:26:33 -0500

    added solution

--- src/Lc1423_MaximumPointsFromCards_M.java
+++ src/Lc1423_MaximumPointsFromCards_M.java
@@ -1,22 +1,23 @@
 public class Lc1423_MaximumPointsFromCards_M {
 
+    // 100% we add up all the values to k-1 from left side
+    // afterwards we subtract the the last digit in this window and add the right side
     public static int maxScore(int[] cardPoints, int k) {
-int ans = 0, leftCounter = 0,  rightcounter=0;
-int len=cardPoints.length;
-int x=k-1;
+        int ans = 0, leftCounter = 0, rightcounter = 0;
+        int len = cardPoints.length;
+        int x = k-1;
         for (int end = 0; end < k; end++) {
             leftCounter += cardPoints[end];
 
         }
-        ans= leftCounter;
-        for (int end = len-1; end > len-1-k ; end--) {
+        ans = leftCounter;
+        for (int end = len - 1; end > len - 1 - k; end--) {
 
-            rightcounter+= cardPoints[end];
-            ans=Math.max(ans, leftCounter-cardPoints[x--]+rightcounter);
-            if (ans > leftCounter) {
-                leftCounter -= rightcounter;
+            rightcounter += cardPoints[end];
+            //updating leftcounter by subtracting the previous value
+            leftCounter -= cardPoints[x--];
+            ans = Math.max(ans, leftCounter  + rightcounter);
 
-            }
 
 
         }
@@ -26,7 +27,7 @@ int x=k-1;
 
     public static void main(String[] args) {
 
-    int[] arr= {1,2,3,4,5,6,1};
+        int[] arr = {1, 2, 3, 4, 5, 6, 1}; // answer should be
 
         System.out.println(maxScore(arr, 3));
 
