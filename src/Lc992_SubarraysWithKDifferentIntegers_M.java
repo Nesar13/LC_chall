7e8e887310b1a06c5e30b1603fd7009ed5373381
Nesar Ahmed <nesar13@hotmail.com>
2020-12-18 17:13:47 -0500

    added solution

--- src/Lc992_SubarraysWithKDifferentIntegers_M.java
+++ src/Lc992_SubarraysWithKDifferentIntegers_M.java
@@ -2,25 +2,33 @@ import java.util.HashSet;
 
 public class Lc992_SubarraysWithKDifferentIntegers_M {
 
+
+    // prefix keeps track of possible distinct integers
     public static int subarraysWithKDistinct(int[] A, int K) {
         int res = 0, prefix = 0;
         int[] m = new int[A.length + 1];
-        for (int i = 0, j = 0, cnt = 0; i < A.length; ++i) {
-            if (m[A[i]]++ == 0) ++cnt;
-            if (cnt > K) {
-                --m[A[j++]]; --cnt; prefix = 0;
+        for (int end = 0, start = 0, count = 0; end < A.length; ++end) {
+            if (m[A[end]]++ == 0) ++count;
+            if (count > K) {
+                --m[A[start++]]; --count; prefix = 0;
             }
-            while (m[A[j]] > 1) {
-                ++prefix; --m[A[j++]];
+            while (m[A[start]] > 1) {
+                ++prefix; --m[A[start++]];
             }
-            if (cnt == K) res += prefix + 1;
+            if (count == K) res += prefix + 1;
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 1, 3, 2};
+        int[] arr = {1, 2, 1, 2, 3};
 
         System.out.println(subarraysWithKDistinct(arr, 2));
+
+        int a=0;
+        int b=0;
+        System.out.println( a++); // 0
+        System.out.println(++b); // 1
+        System.out.println(a); // 1
     }
 }
