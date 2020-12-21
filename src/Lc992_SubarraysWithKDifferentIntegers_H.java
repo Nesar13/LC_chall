2698977731950daee9b8e0423c2f924342f3865b
Nesar Ahmed <nesar13@hotmail.com>
2020-12-21 16:12:52 -0500

    Changed name

--- src/Lc992_SubarraysWithKDifferentIntegers_H.java
+++ src/Lc992_SubarraysWithKDifferentIntegers_H.java
@@ -1,19 +1,20 @@
 import java.util.HashSet;
 
-public class Lc992_SubarraysWithKDifferentIntegers_M {
+public class Lc992_SubarraysWithKDifferentIntegers_H {
 
 
-    // prefix keeps track of possible distinct integers
+    // prefix keeps track of possible solutions in a subarray
+    // 100%
     public static int subarraysWithKDistinct(int[] A, int K) {
         int res = 0, prefix = 0;
-        int[] m = new int[A.length + 1];
+        int[] freqArr = new int[A.length + 1];
         for (int end = 0, start = 0, count = 0; end < A.length; ++end) {
-            if (m[A[end]]++ == 0) ++count;
+            if (freqArr[A[end]]++ == 0) ++count;
             if (count > K) {
-                --m[A[start++]]; --count; prefix = 0;
+                --freqArr[A[start++]]; --count; prefix = 0;
             }
-            while (m[A[start]] > 1) {
-                ++prefix; --m[A[start++]];
+            while (freqArr[A[start]] > 1) {
+                ++prefix; --freqArr[A[start++]];
             }
             if (count == K) res += prefix + 1;
         }
@@ -22,6 +23,8 @@ public class Lc992_SubarraysWithKDifferentIntegers_M {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 1, 2, 3};
+        // 5,7,5,2,3,3,4,1,5,2,7,4,6,2,3,8,4,5,7
+        // K = 7
 
         System.out.println(subarraysWithKDistinct(arr, 2));
 
