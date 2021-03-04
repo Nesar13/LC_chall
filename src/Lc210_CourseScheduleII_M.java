2edcd656935a981f6f5502a4383073ba9820f56f
Nesar Ahmed <nesar13@hotmail.com>
2021-03-04 17:46:58 -0500

    Lc210_CourseScheduleII_M.java did not finish

--- src/Lc210_CourseScheduleII_M.java
+++ src/Lc210_CourseScheduleII_M.java
@@ -6,26 +6,60 @@ public class Lc210_CourseScheduleII_M {
 
     public static void main(String[] args) {
 
+        // [[1,0],[2,0],[3,1],[3,2]]
+        // [0,1,2,3,]
+        //
+
+        int [][] arr= {{1,0},{2,0},{3,1},{3,2}};
+        System.out.println(findOrder(4, arr));
+
+
     }
 
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-       ArrayList[] graph = new ArrayList[numCourses];
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
 
-        int[] prereqFreq = new int[numCourses];
+        int[] res = new int[numCourses];
 
-        Boolean [] visited=new Boolean[numCourses];
+        Boolean[] visited = new Boolean[numCourses];
+        Boolean[] dp = new Boolean[numCourses];
 
-        for (int i = 0; i < prerequisites.length; i++) {
-            graph[i]=new ArrayList();
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList();
         }
 
         for (int i = 0; i < prerequisites.length; i++) {
-            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+            graph[prerequisites[i][0]].add(prerequisites[i][1]);
 
         }
 
-        return prereqFreq;
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, prerequisites, visited, dp, res, graph, 0)) {
 
+                int[] temp = new int[0];
+                return temp;
+            }
 
+        }
+        return res;
+    }
+
+    private static boolean dfs(int course, int[][] prerequisites, Boolean[] visited, Boolean[] dp, int[] res, ArrayList[] graph, int count) {
+
+        if (visited[course]) {
+            return dp[course];
+        } else visited[course] = true;
+        for (int i = 0; i < graph[course].size(); i++) {
+
+            if (!dfs((int) graph[course].get(i), prerequisites, visited, dp, res, graph, count)) {
+
+                dp[course] = false;
+                return false;
+            } else res[count++] = (int) graph[course].get(i);
+
+        }
+
+        dp[course] = true;
+        return true;
     }
 }
