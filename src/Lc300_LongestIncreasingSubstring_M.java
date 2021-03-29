9469235b08cf30313bb375384c086dd5ca2524a8
Nesar Ahmed <nesar13@hotmail.com>
2021-03-29 16:59:12 -0400

    added solution to Lc300_LongestIncreasingSubstring_M.java

--- src/Lc300_LongestIncreasingSubstring_M.java
+++ src/Lc300_LongestIncreasingSubstring_M.java
@@ -126,20 +126,19 @@ public class Lc300_LongestIncreasingSubstring_M {
         return maxans;
     }
 
-        public static int lengthOfLIS5(int[] nums) {
-            int[] dp = new int[nums.length];
-            int len = 0;
-            for (int num : nums) {
-                int i = Arrays.binarySearch(dp, 0, len, num);
-                if (i < 0) {
-                    i = -(i + 1);
-                }
-                dp[i] = num;
-                if (i == len) {
-                    len++;
-                }
+    public static int lengthOfLIS5(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
             }
-            return len;
-
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
     }
 }
