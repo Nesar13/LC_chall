fc8e88cd66586e49dccd2eb2c96596f29d617182
Nesar Ahmed <nesar13@hotmail.com>
2021-04-16 14:17:28 -0400

    Lc98_ValidateBinarySearchTree_M.java

--- src/Lc98_ValidateBinarySearchTree_M.java
+++ src/Lc98_ValidateBinarySearchTree_M.java
@@ -3,7 +3,7 @@ public class Lc98_ValidateBinarySearchTree_M {
     //           5
     //          / \
     //         3   6
-    // For this instance, we have to ensure that 5 is < the MAX_VALUE, and greater than MIN VALUE
+    // For example, we have to ensure that 5 is < the MAX_VALUE, and greater than MIN VALUE
     // 3 would have to be less than 5 which is the max value now  and greater than min value
     // so we recursively put this restriction on each node and return true or false
     // TC: O(N) ….SC:O(d) d is the depth since we are calling to the depth on the call stack
