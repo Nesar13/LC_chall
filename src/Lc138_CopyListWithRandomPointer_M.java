b94f33a5fcee7e5433a88b707817a6bdc912aeda
Nesar Ahmed <nesar13@hotmail.com>
2021-04-30 17:27:27 -0400

    added solution for Lc138_CopyListWithRandomPointer_M

---
+++ src/Lc138_CopyListWithRandomPointer_M.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+
+public class Lc138_CopyListWithRandomPointer_M {
+
+        // we store the node in a hashmap as key and create  a new node with it's value
+    // we will update all the values with the the values of the keys
+    // note that we store a new node as a value in the hashtable
+    public Node copyRandomList(Node head) {
+
+        // HashMap which holds old nodes as keys and new nodes as its values.
+        HashMap<Node, Node> map = new HashMap<Node, Node>();
+
+        //copy list nodes into hash table
+        Node p = head;
+        while (p != null) {
+            map.put(p, new Node(p.val));
+            p = p.next;
+        }
+
+        // deep copy using hash table
+        Node q = head;
+        while (q != null) {
+            map.get(q).next = map.get(q.next);
+            map.get(q).random = map.get(q.random);
+            q = q.next;
+        }
+
+        // return new head
+        return map.get(head);
+    }
+
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+}
