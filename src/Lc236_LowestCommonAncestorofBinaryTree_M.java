d0d9d06b121d8fd5c05e13ffd598d779b70a2148
Nesar Ahmed <nesar13@hotmail.com>
2021-05-06 17:24:35 -0400

    added solution to Lc236_LowestCommonAncestorofBinaryTree_M.java

--- src/Lc236_LowestCommonAncestorofBinaryTree_M.java
+++ src/Lc236_LowestCommonAncestorofBinaryTree_M.java
@@ -26,12 +26,14 @@ public class Lc236_LowestCommonAncestorofBinaryTree_M {
         int right = helper(root.right, p, q) ? 1 : 0;
 
         // we will check if the current node matches p or q
+        //mid is basically to make sure we check our initial condition
+        // is set to 1
         int mid = (root == p || root == q) ? 1 : 0;
 
         // we add up the tally, our answer is if they're more than two
         if (left + right + mid >= 2) ans = root;
 
-
+        // here
         return (left + right + mid) > 0;
     }
 }
