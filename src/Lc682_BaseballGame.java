f2cbeb38e9166053ec47859dc9c7eb26ff43ed2f
Nesar Ahmed <nesar13@hotmail.com>
2021-05-28 13:32:18 -0400

    added solution Lc682_BaseballGame

---
+++ src/Lc682_BaseballGame.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Lc682_BaseballGame {
+    public int calPoints(String[] ops) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newtop = top + stack.peek();
+                stack.push(top);
+                stack.push(newtop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(op));
+            }
+        }
+
+        int ans = 0;
+        for(int score : stack) ans += score;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
