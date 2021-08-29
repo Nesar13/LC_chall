c37c8c768ef30e87f1ca8c4cec7c84d821702539
Nesar Ahmed <nesar13@hotmail.com>
2021-08-28 20:42:29 -0400

    added comment

--- src/Lc759_EmployeeFreeTime_H.java
+++ src/Lc759_EmployeeFreeTime_H.java
@@ -1,13 +1,50 @@
 import java.util.*;
 
 public class Lc759_EmployeeFreeTime_H {
-    public class Interval {
+    public static class Interval {
       int start;
       int end;
       Interval() { start = 0; end = 0; }
      Interval(int s, int e) { start = s; end = e; }
-  }
-    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+
+
+        @Override
+        public String toString() {
+            return
+                     start +
+                    ", " + end ;
+
+        }
+    }
+// we sort the list by start time by adding to our priority queue
+    // and then we iterate through our queue to check for any free time (meaning there's no overlap)
+    // we have a running maxCurrEndTIme to keep track of the previous end time
+    // if the current start time is greater than our max and it's not the first list
+    // we can add it to our result
+    // we update the currMaxEndTime by taking the max of (max, end)
+    // O(nlogn) time for sorting and adding to our queue | O(n) space
+    public static List < Interval > employeeFreeTime(List<List<Interval >> schedule) {
+        PriorityQueue < Interval > que = new PriorityQueue < > (Comparator.comparingInt(a -> a.start));
+
+        for (List < Interval > list: schedule) {
+            for (Interval i: list) {
+                que.add(i);
+            }
+        }
+
+        List < Interval > res = new ArrayList < > ();
+        int currMaxEndTime = -1;
+        while (!que.isEmpty()) {
+            Interval top = que.poll();
+            if (currMaxEndTime != -1 && top.start > currMaxEndTime) {
+                res.add(new Interval(currMaxEndTime, top.start));
+            }
+            currMaxEndTime = Math.max(currMaxEndTime, top.end);
+        }
+
+        return res;
+    }
+/*    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> res = new ArrayList<>();
 
         if (schedule == null || schedule.size() == 0) {
@@ -39,9 +76,32 @@ public class Lc759_EmployeeFreeTime_H {
             endTime.add(list.get(i).end);
         }
         return res;
-    }
+    }*/
 
     public static void main(String[] args) {
+//[[[1,2],[5,6]],[[1,3]],[[4,10]]]
+        Interval one=new Interval(1,2) ;
+        Interval two=new Interval(5,6) ;
+
+        List<Interval> l=new ArrayList<>();
+        l.add(one);
+        l.add(two);
+
+        List<Interval> l2= new ArrayList<>();
+        l2.add(new Interval(1,3));
+
+        List<Interval> l3= new ArrayList<>();
+        l3.add(new Interval(4,10));
+
+
+        List<List<Interval>> main=new ArrayList<>();
+        main.add(l);
+        main.add(l2);
+        main.add(l3);
+
+
+        System.out.println(employeeFreeTime(main));
+
 
     }
 }
