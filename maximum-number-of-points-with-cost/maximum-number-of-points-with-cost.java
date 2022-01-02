0f5f1711499ef352b2b34168de89486d3caef076
Nesar Ahmed <nesar13@hotmail.com>
2022-01-01 22:05:47 -0500

    Time: 11 ms (61.40%), Space: 121.4 MB (15.66%) - LeetHub

--- maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.java
+++ maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.java
@@ -4,12 +4,15 @@ class Solution {
         int m = points[0].length;
         long[] dp = new long[m];
         for (int[] point : points) {
+            // adding points to current dp
             for (int j = 0; j < m; j++) {
                 dp[j] += point[j];
             }
+            // computing left side 
             for (int j = 1; j < m; j++) {
                 dp[j] = Math.max(dp[j], dp[j - 1] - 1);
             }
+            // computing right side 
             for (int j = m - 2; j >= 0; j--) {
                 dp[j] = Math.max(dp[j], dp[j + 1] - 1);
             }
