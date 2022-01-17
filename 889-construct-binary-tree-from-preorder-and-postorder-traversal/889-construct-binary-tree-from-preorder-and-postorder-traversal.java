6be39130ba234b49b710e498dccc565bb207c195
Nesar Ahmed <nesar13@hotmail.com>
2022-01-17 00:55:36 -0500

    Time: 1 ms (74.98%), Space: 38.1 MB (96.44%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -15,60 +15,27 @@
  */
 class Solution {
     
-    //
-    
-    
-//     private int preindex = 0;
-// public TreeNode constructFromPrePost(int[] pre, int[] post) {
-//     HashMap<Integer, Integer> postMap = new HashMap<>();
-//     for(int i = 0; i < post.length; i++)
-//         postMap.put(post[i], i);
-//     return helper(pre, postMap, 0, post.length-1);
-// }
-
-// private TreeNode helper(int[] pre, HashMap<Integer, Integer> postMap, int low, int high) {
-//     if(low > high)
-//         return null;
-    
-//     TreeNode node = new TreeNode(pre[preindex++]);
-//     if(low == high)
-//         return node;
-    
-//     int mid = postMap.get(pre[preindex]);
-//     node.left = helper(pre, postMap, low, mid);
-//     node.right = helper(pre, postMap, mid+1, high-1);
-//     return node;
-// }
+   
     HashMap<Integer, Integer> postMap;
     int preIndex=0; 
     public TreeNode constructFromPrePost(int[] pre, int[] post) {
         
         postMap=new HashMap<>(); 
-        for (int i=0; i < post.length; i++){
-            
+        for (int i=0; i < post.length; i++)  
             postMap.put(post[i], i); 
-        }
-        
-     
-        
         
        return helper( pre, post,  0, post.length-1); 
-       
-
     }
     
     
     
     TreeNode helper(int[] pre, int[] post, int i, int j){
-        
-        
         if (i >j) return null;
-        
-      
         TreeNode root=new TreeNode(pre[preIndex++]); 
        
         if (i==j) return root; 
-        int mid=postMap.get(pre[preIndex]); 
+        int mid=postMap.get(pre[preIndex]);
+        
         root.left=helper(pre, post, i, mid); 
         root.right=helper(pre, post, mid+1, j-1);
         
