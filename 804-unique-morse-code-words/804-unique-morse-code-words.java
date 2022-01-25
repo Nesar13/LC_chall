91b8a751cbc03ea0a1fcf608024ea06963787fe8
Nesar Ahmed <nesar13@hotmail.com>
2022-01-25 16:34:02 -0500

    Time: 1 ms (100.00%), Space: 36.9 MB (87.51%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -9,9 +9,10 @@ class Solution {
         
         HashSet<String> set=new HashSet<>(); 
         int res=0; 
-        StringBuilder sb=new StringBuilder(); 
+         
         for (String w: words){
-            sb.setLength(0); 
+            //sb.setLength(0); 
+            StringBuilder sb=new StringBuilder();
             for (int i=0; i < w.length(); i++){
                 sb.append(MORSE[w.charAt(i)-'a']); 
                 
