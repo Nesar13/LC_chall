77b0a2358d3e160b6754cc1635216b4a1f51431d
Nesar Ahmed <nesar13@hotmail.com>
2022-01-29 12:50:59 -0500

    Time: 5 ms (78.15%), Space: 43.2 MB (11.00%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,5 +1,10 @@
 class Solution {
-    
+    // 1) I intially thought we can just store frequency
+    // and if the char was greater or equal to number of words
+    // we can just add to our result
+    // 2) This assumption is incorrect since we can have 
+    // many of the same chars in one word
+    // so we have to get the min frequency of each char in each word
     public List<String> commonChars(String[] words) {
         int[] freq=new int[26]; 
         Arrays.fill(freq, Integer.MAX_VALUE); 
@@ -18,7 +23,7 @@ class Solution {
         
         List<String> res=new ArrayList(); 
         // we know that the min value has to be at least 1 or greater
-        // since we got the min frequency of all the strings 
+        // since we got the min frequency of chars of all the strings 
         for (int i=0; i< 26; i++){
             
             int curr=freq[i]; 
