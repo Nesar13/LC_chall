0dab723023a2cc2a16d3584ea50d36fe03f73f82
Nesar Ahmed <nesar13@hotmail.com>
2022-01-30 16:03:30 -0500

    Time: 1 ms (69.13%), Space: 44.3 MB (5.13%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.java
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -2,9 +2,7 @@ class Solution {
     public int countBattleships(char[][] board) {
         
         int rows=board.length; 
-        int cols=board[0].length; 
-        int[] row=new int[rows]; 
-        int[] col=new int[cols]; 
+        int cols=board[0].length;
         int res=0; 
         
         for (int i=0; i < rows; i++){
