aa26d2c6dd52e45af2f1f51f8dd3470001ac39f6
Nesar Ahmed <nesar13@hotmail.com>
2022-01-31 15:20:47 -0500

    Time: 28 ms (55.80%), Space: 54.7 MB (16.44%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,20 +1,24 @@
 class Solution {
-       public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> res = new ArrayList<>();
-        for (int i=0; i < l.length; i++) {
-            res.add(checkIsArithmeticSubarrays(Arrays.copyOfRange(nums, l[i], r[i]+1)));
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> list = new ArrayList<>();
+        for(int i=0; i<l.length; i++){
+            int strt = l[i];
+            int end = r[i];
+            int[] arr = new int[end-strt+1];
+            int k=0;
+            for(int j=strt; j<=end; j++){
+                arr[k++] = nums[j]; 
+            }
+            list.add(isArithmetic(arr));
         }
-        return res;
+        return list;
     }
-    
-    public boolean checkIsArithmeticSubarrays(int[] nums) {
-        if (nums.length < 2) return false;
-        if (nums.length == 2) return true;
-        Arrays.sort(nums);
-        int delta = nums[1]-nums[0];
-        for (int i=1; i < nums.length; i++) {
-            if (nums[i]-nums[i-1] != delta) return false;
-        }
+    private boolean isArithmetic(int[] arr){
+        Arrays.sort(arr);
+        int d = arr[1]-arr[0];
+        for(int i=1; i<arr.length; i++)
+            if(arr[i]-arr[i-1] != d)
+                return false;
         return true;
     }
 }
