4b39e695c11493756e238467494bb2b2eb05bf39
Nesar Ahmed <nesar13@hotmail.com>
2022-01-31 15:26:54 -0500

    Time: 75 ms (15.63%), Space: 72.7 MB (5.12%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,24 +1,52 @@
 class Solution {
-    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> list = new ArrayList<>();
-        for(int i=0; i<l.length; i++){
-            int strt = l[i];
-            int end = r[i];
-            int[] arr = new int[end-strt+1];
-            int k=0;
-            for(int j=strt; j<=end; j++){
-                arr[k++] = nums[j]; 
-            }
-            list.add(isArithmetic(arr));
+      public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> res = new ArrayList<Boolean>();
+        for(int i = 0; i < l.length; i++) {
+            res.add(isArithmeticSeq(nums, l[i], r[i]));
         }
-        return list;
+        
+        return res;
     }
-    private boolean isArithmetic(int[] arr){
-        Arrays.sort(arr);
-        int d = arr[1]-arr[0];
-        for(int i=1; i<arr.length; i++)
-            if(arr[i]-arr[i-1] != d)
-                return false;
-        return true;
+
+
+private boolean isArithmeticSeq(int[] nums, int start, int end) {
+    //If the subarray is size 1(never less because l[i] < r[i] is a given), it's an arithmetic sequence.
+    if(end - start < 2) return true; 
+    
+    //The first goal is to find the maximum value and the minimum value in the subarray.
+    //If the subarray is an arithmetic sequence, there should be an even gap between every consecutive index.
+    int min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
+
+    //The order of the numbers doesn't matter, so we can use a set to hold all values of the subarray.
+    //Add all values between the left and right indexes into the set.
+    Set<Integer> set = new HashSet<Integer>();
+    for(int i = start; i <= end; i++) {
+        min = Math.min(min, nums[i]);
+        max = Math.max(max, nums[i]);
+        set.add(nums[i]);
     }
+    
+    //If we cannot even divide the difference between the max and min, we don't have an arithmetic sequence.
+    //There needs to be an interval that we can add to each index to get the next number.
+
+    //Example1 - SubArray(Not an arithmetic seq) = {5, 10, 15, 20, 27}
+    //Max = 27, Min = 5, Start = 0, End = 4
+    //Interval = (27 - 5) / (4 - 0) = 22 / 4 = 5.5
+
+    //Example2 - SubArray(Arithmetic seq) = {5, 10, 15, 20, 25}
+    //Max = 25, Min = 5, Start = 0, End = 4
+    //Interval = (25 - 5) / (4 - 0) = 20 / 4 = 5
+
+    if((max - min) % (end - start) != 0) return false;
+    int interval = (max - min) / (end -start);
+    
+    //Because we know what the interval between each number must be, we can search the set to
+    //to confirm whether or not it's in the subarray.
+    for(int i = 1; i <= end -start; i++) {
+        if(!set.contains(min + i * interval)) return false;
+    }
+    
+    return true;
+}
+
 }
