4c837e543f473db180cb93fa7d62884a0a48563c
Nesar Ahmed <nesar13@hotmail.com>
2022-01-31 15:20:06 -0500

    Time: 39 ms (26.68%), Space: 54.4 MB (16.98%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,31 +1,20 @@
 class Solution {
-    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> res = new ArrayList<Boolean>();
-        for(int i = 0; i < l.length; i++) {
-            res.add(isArithmeticSeq(nums, l[i], r[i]));
+       public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> res = new ArrayList<>();
+        for (int i=0; i < l.length; i++) {
+            res.add(checkIsArithmeticSubarrays(Arrays.copyOfRange(nums, l[i], r[i]+1)));
         }
-        
         return res;
     }
     
-    private boolean isArithmeticSeq(int[] nums, int start, int end) {
-        if(end - start < 2) return true;
-        
-        int min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i = start; i <= end; i++) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
-            set.add(nums[i]);
+    public boolean checkIsArithmeticSubarrays(int[] nums) {
+        if (nums.length < 2) return false;
+        if (nums.length == 2) return true;
+        Arrays.sort(nums);
+        int delta = nums[1]-nums[0];
+        for (int i=1; i < nums.length; i++) {
+            if (nums[i]-nums[i-1] != delta) return false;
         }
-        
-        if((max - min) % (end - start) != 0) return false;
-        int interval = (max - min) / (end -start);
-        
-        for(int i = 1; i <= end -start; i++) {
-            if(!set.contains(min + i * interval)) return false;
-        }
-        
         return true;
     }
 }
