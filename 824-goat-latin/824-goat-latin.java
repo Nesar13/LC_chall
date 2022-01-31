156bafa3c6839621fd2dffd121334af885612a96
Nesar Ahmed <nesar13@hotmail.com>
2022-01-30 22:56:12 -0500

    Time: 15 ms (16.33%), Space: 43.1 MB (5.97%) - LeetHub

--- 824-goat-latin/824-goat-latin.java
+++ 824-goat-latin/824-goat-latin.java
@@ -1,14 +1,14 @@
 class Solution {
        public String toGoatLatin(String S) {
         Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
-        String res = "";
+        StringBuilder sb= new StringBuilder(); 
         int i = 0, j = 0;
         for (String w : S.split("\\s")) {
-            res += ' ' + (vowels.contains(w.charAt(0)) ? w : w.substring(1) + w.charAt(0)) + "ma";
+            sb.append( ' ' + (vowels.contains(w.charAt(0)) ? w : w.substring(1) + w.charAt(0)) + "ma");
             for (j = 0, ++i; j < i; ++j) {
-                res += "a";
+               sb.append( "a");
             }
         };
-        return res.substring(1);
+        return sb.substring(1).toString();
     }
 }
