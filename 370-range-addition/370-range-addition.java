63fc3ad279abbed251fb6b138d820c9b1cbf5209
Nesar Ahmed <nesar13@hotmail.com>
2022-03-10 14:39:32 -0500

    Time: 2 ms (88.29%), Space: 55.5 MB (37.94%) - LeetHub

--- 370-range-addition/370-range-addition.java
+++ 370-range-addition/370-range-addition.java
@@ -1,30 +1,36 @@
 class Solution {
     
     
-    // we can have 1, 10
-    // 
-    // 0 1 2 3 4 5 7
-    //   2 3 2 3
+  
+    // 1) I intially thought we can store the ranges, but I didn't know how to implement it
+    // 2) The trick is to add the increment to the start value and subtract the increment
+    // from the end+1 index
+    // 3) aftwards, you do a running sum and set each sum to our array
+    // so the ith index should be the sum of all the times this was updated
+    // lenght: 5
+    //Input: [[1,3,2],[2,4,3],[0,2,-2]]
+    // 0 1 2 3 4
+    // 0 0 0 0 0
+    //   2    -2
+    //     3  
+    // -2    2
+    // -2 0 3 5 3  <- output
+    // O(n+k) where n is length of array, and k is the length of the given updates
+    // O(1) time
     public int[] getModifiedArray(int length, int[][] updates) {
-        
-        int[] arr=new int[length]; 
-        
-        for (int[] u: updates){
-            
-            int start=u[0]; 
-            int end=u[1]; 
-            int inc=u[2]; 
-            for (int i=start; i <=end ; i++){
-                arr[i]+=inc; 
-            }
-            
-            
+        int[] res = new int[length];
+        for (int[] update : updates) {
+            int start = update[0], end = update[1], val = update[2];
+            res[start] += val;
+            if (end < length - 1) res[end + 1] -= val;
         }
-        
-        
-        return arr; 
-        
-        
-        
+        int cur = 0;
+        // "range caching"
+        for (int i = 0; i < length; i ++) {
+            cur += res[i];
+            res[i] = cur;
+        }
+        return res;
     }
+
 }
