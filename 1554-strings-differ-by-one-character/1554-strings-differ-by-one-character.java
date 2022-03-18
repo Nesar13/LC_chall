cb4046361eeb4b1af1b507cbb1f4353bc6ffa20c
Nesar Ahmed <nesar13@hotmail.com>
2022-03-17 21:22:43 -0400

    Time: 171 ms (68.54%), Space: 48.4 MB (89.05%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
@@ -1,33 +1,19 @@
 class Solution {
-    public boolean differByOne(String[] dict) {
-        
-        if (dict.length==0) return true; 
-        HashSet<String> set=new HashSet(); 
-        
-        for (String s: dict){
-            set.add(s); 
-        }
-        
-        for (String s: dict){
-            
-            for(int i=0; i < s.length(); i++){
-               set.remove(s); 
-                for (char c='a' ; c <= 'z' ; c++){
-                    StringBuilder sb=new StringBuilder();  
-                    sb.append(s.substring(0, i)).append(c).append(s.substring(i+1)); 
-                    if (set.contains(sb.toString())) return true; 
+   public boolean differByOne(String[] dict) {
+        Set<String> set = new HashSet<>();
+        int len = dict[0].length();
+        for(int i = 0; i < len; i++){
+            set.clear();
+            StringBuilder sb; 
+            for(String str: dict){
+                sb=new StringBuilder(); 
+                sb.append(str.substring(0, i)).append(str.substring(i + 1, len));
+                if(set.contains(sb.toString())){
+                    return true;
                 }
-                set.add(s); 
-
-                
+                set.add(sb.toString());
             }
-            
-            
         }
-        
-        return false; 
-        
-        
-        
+        return false;
     }
 }
