69e26eaac5f02ad5378608c73eb7b52657f17e01
Nesar Ahmed <nesar13@hotmail.com>
2022-04-04 12:55:00 -0400

    Time: 4 ms (99.19%), Space: 63 MB (93.93%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,22 +1,32 @@
 class Solution {
-
-    public int[] rearrangeArray(int[] nums) {
-        //We want to sort the number then alternate between a smallest
-        //number and biggest number to grantee that they will never be averaged.
-        Arrays.sort(nums);
-        int l=0, r=nums.length-1, len=nums.length;
-        int[] res = new int[len];
-        for(int i=0; i<len; i++) {
-            //move left and increment counter to next index for right
-            res[i++] = nums[l];
-            l++;
-            // only move right if less then left
-            if(l<=r) {
-                res[i] = nums[r];
-                r--;
-            }
+    // 1) We can sort this and rearrange the array to have the biggest number
+    // in the middle , but this would take O(nlogn) time | O(1) space
+    // 2) We can do this in O(n) time 
+    // 3) At its core, this approach is based on the fact 
+    //that the average of two numbers can never be 
+    //lower than the smallest of the two elements, 
+    //nor higher than the largest of the two elements. 
+    //      -This means that if an element's neighbors are both 
+    //       smaller than itself, then the average of those two neighbors 
+    //       will be smaller than the element in question. 
+    //      -The same is true for larger elements as well, though with the average 
+    //       being larger than the element in question. 
+    //So what we can do is go through the array and, in a sense, 
+    //rearrange it into a series of peaks and valleys, that way ensuring that we meet the no average criteria.
+    
+    // for example: take 2,3,4 
+    // we know we need to rearrange 3 and 4 to make 4 the highest number so it'll always be bigger than avg
+    // Another example: 4,3,2, we know we need to swap 3 and 2 to make the 2 the smallest 
+    // number in the middle 
+public int[] rearrangeArray(int[] nums) {
+    for(int i = 1; i < nums.length - 1; i++){
+        if((nums[i - 1] < nums[i] && nums[i] < nums[i + 1]) || (nums[i - 1] > nums[i] && nums[i] > nums[i + 1])){
+            int tmp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = tmp;
         }
-        return res;
     }
+    return nums;
+}
     
 }
