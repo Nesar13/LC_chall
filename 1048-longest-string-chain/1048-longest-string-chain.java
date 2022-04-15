20fa111d0ed7015807ec2b09b07dd64f8a5d3876
Nesar Ahmed <nesar13@hotmail.com>
2022-04-15 18:32:37 -0400

    Time: 38 ms (81.36%), Space: 42.4 MB (85.56%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,34 +1,33 @@
 class Solution {
-    // The trick is traversing the graph in reverse order. 
-    //Like that "bac" -> "bc" -> "b". It is easier to calculate next candidate children in reverse order.
-    //For example from "b" -> ... you should try all alphabet letters but in reverse order you can only remove one letter "bc" -> "b" or "bc" -> "c".      //Time Complexity = O(NxL), N is the words count, L is the length of words. 
-    //Space Complexity = O(NxL) for storing set of words and score are    same. (Recursive stack can be O(N) space at most.)
-       private int backtrack(HashSet<String> set, HashMap<String, Integer> map, String str) {
-        //Integer pathLen = map.get(str);
-         if (map.containsKey(str)) return map.get(str); 
-        int res = 1;
-        String sb = null;
-        for(int i=0; i<str.length(); i++) {
-            sb = str.substring(0, i) + str.substring(i+1, str.length());
-            if(set.contains(sb)) {
-                res = Math.max(1+backtrack(set, map, sb), res);
-            }
-        }
-        map.put(str, res);
-        return res;
-    }
     
+    // bdca -> bca -> ba ->b
     public int longestStrChain(String[] words) {
         HashSet<String> set = new HashSet<>();
         HashMap<String, Integer> map = new HashMap<>();
         for(String word:words){
             set.add(word);
         }
-        int ans = 1;
-        for(String word: words){
-            ans = Math.max(ans, backtrack(set, map, word));
+        int res = 1;
+        
+        for (int i=0; i< words.length; i++) res=Math.max(res, backtrack(set, map,  words[i])) ; 
+        
+        return res; 
+       
+    }
+    
+    
+    
+    int backtrack(HashSet<String> set, HashMap<String, Integer> map, String curr){
+        if (map.containsKey(curr)) return map.get(curr);
+        
+   
+        int count=1; 
+        for (int i=0; i < curr.length(); i++){
+            String temp=curr.substring(0, i) + curr.substring(i+1); 
+            if (set.contains(temp)) count=Math.max(count, 1+backtrack(set, map, temp));                                          
         }
-        return ans;
+        map.put(curr, count); 
+        return count; 
     }
     
      
