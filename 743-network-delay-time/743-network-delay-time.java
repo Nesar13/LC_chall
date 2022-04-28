499f5f9ff76e1f21d90d7c1a6b17dc9c482d6bcd
Nesar Ahmed <nesar13@hotmail.com>
2022-04-28 02:35:19 -0400

    Time: 58 ms (12.19%), Space: 72.8 MB (5.14%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,4 +1,19 @@
 class Solution {
+    
+    //We use dijkstra's path finding algorithm to get 
+    // the fastest path to all nodes
+    // we create an adjacency list : HashMap<Integer, List<List>>
+    // so it would be like this: node: {neighbor, timeItTakes}
+    // we have to use minheap per dijjstra's ago so we always have the min time it takes
+    // and once we visit all nodes, we can return the total time
+    // our min heap will look like this with input
+    //times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+    //  1 <- 2 -> 3 -> 4
+    // minheap: [2,0] , [1,1], [3,1], [4,2]
+    // notice how the second value is sorted and added from previous values
+    // output would be 2 since we will have visited all nodes at 4 with time 2
+    // O(V+ELogV) time | O(V+E) space
+    
     public int networkDelayTime(int[][] times, int n, int k) {
         
         HashMap<Integer, List<List<Integer>>> graph=new HashMap<>(); 
@@ -11,18 +26,10 @@ class Solution {
             graph.get(t[0]).add(new ArrayList<>(temp)); 
             
         }
-        // notice that we set our graph with all nodes
-        // we will get an error when we try to get a value that doesn't exist in
-        // map
-        //for (int i=1; i <=n ; i++ ) graph.putIfAbsent(i, new ArrayList<>()); 
-        
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1])); 
         HashSet<Integer> visited=new HashSet<>(); 
         
-        pq.offer(new int[]{k,0}); 
-        
-        int res=Integer.MAX_VALUE; 
-        
+        pq.offer(new int[]{k,0}); // we insert into pq [node, time]
         while (!pq.isEmpty()){
             
             int s=pq.size(); 
@@ -35,23 +42,16 @@ class Solution {
             // we can just return the first time where we visit all nodes, since that will be the smallest time
             if(visited.size() ==n) return currTime; 
             List<List<Integer>> neighbors=graph.get(currNode); 
-            if (neighbors != null) {
+            if (neighbors != null) { // we have to check if null since not all the nodes may be keys in our graph
             for (List<Integer> neighbor: neighbors){
                 if (visited.contains((neighbor.get(0)))) continue; 
-
                 pq.offer(new int[]{neighbor.get(0), neighbor.get(1)+currTime}); 
-
-
+                }
             }
-            }
-                
-          
-            
+
         }
-        
-        
+
         //return visited.size()==n? res: -1; 
         return -1;
-        
     }
 }
