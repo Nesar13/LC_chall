16879eea6c57e0981552573b1bb7bd08aff517bd
Nesar Ahmed <nesar13@hotmail.com>
2022-06-07 20:10:11 -0400

    Time: 7 ms (82.18%), Space: 43.2 MB (79.62%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -1,12 +1,19 @@
 class Solution {
+    
+    //1)  we have to realize that we have to store adjacency list in 
+    // hashmap where the key is source and the value is a list of destinations
+    // since the problem requires you to go in sorted order, we have to 
+    // sort the list in our hashmap
+    // HashMap<String, List<String>>
+    // 2) Then we have to use backtracking to generate all possible paths
+    
+    
     public List<String> findItinerary(List<List<String>> tickets) {
         
         List<String> res=new ArrayList<>(); 
         
     
-        
         HashMap<String, List<String>> map=new HashMap<>(); 
-        
         
         for (List<String> t: tickets){
             
@@ -36,7 +43,6 @@ class Solution {
         for (int i=0; i < map.get(curr).size(); i++){
             String to=map.get(curr).get(i); 
             res.add(curr); 
-            
             map.get(curr).remove(i); 
             if (map.get(curr).size()== 0) map.remove(curr);
             if(dfs(to, map, res)) return true; 
