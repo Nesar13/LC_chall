c1a1af26be36fd1811e9ed557b89c932e4b36013
Nesar Ahmed <nesar13@hotmail.com>
2022-06-21 16:44:34 -0400

    Time: 0 ms (100.00%), Space: 43.7 MB (65.36%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,34 +1,47 @@
 class Solution {
-    
-    
-   public int[] findRedundantConnection(int[][] edges) {
-       //max edges is 1000
-        int[] parents = new int[edges.length+1]; 
-       //map index to node
-        for (int i = 0; i < parents.length; i++) parents[i] = i;
-       
-        for (int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-            //return the result
-            // mathematically, the edge that we add that will be created to create a loop will be found first
-            if(find(from, parents) == find(to, parents)) return edge;      
-            union(from, to, parents);
+    public int[] findRedundantConnection(int[][] edges) {
+        
+        // 0 1 2 3 4 5 
+        // 0   1
+        int[] arr=new int[edges.length+1] ; 
+        
+        for (int i=0; i < arr.length; i++)
+            arr[i]=i; 
+
+        for (int[] e: edges){
+            
+            if (find(arr, e[0]) ==find(arr, e[1])) return e; 
+            
+            union(arr, e[0], e[1]); 
+            
         }
-        return null;
+        
+        return new int[]{0,0}; 
+        
+        
     }
-
-    //finds the node your looking for 
-    private int find(int node, int[] parents) {
-        while(node != parents[node])  node = parents[node];
-        return node;
+    
+    void union(int[] arr, int x, int y){
+        
+        int xParent=find(arr, x); 
+        int yParent=find(arr, y); 
+        
+        if (xParent != yParent){
+            arr[yParent]=xParent; 
+        }
+        
+        
     }
-
-    private void union(int a, int b, int[] parents) {
-        int aRoot = find(a, parents);
-        int bRoot = find(b, parents);
-        if(aRoot == bRoot) return;
-        //val for the parent index added to current index
-        else parents[aRoot] = bRoot;
+    
+    
+    
+    
+    int find(int[] arr, int x){
+        
+        while (x != arr[x]){
+            x=arr[x]; 
+        }
+        
+        return x; 
     }
 }
