62be59b08b71aa450411ec5f33aa980d18b2ddb0
Nesar Ahmed <nesar13@hotmail.com>
2022-06-24 20:02:44 -0400

    Time: 4 ms (44.67%), Space: 42.6 MB (75.27%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -1,27 +1,35 @@
 class Solution {
-  public String findReplaceString(String s, int[] indexes, String[] sources, String[] targets) {
-    Map<Integer, Integer> map = new HashMap<>();
-    StringBuilder sb = new StringBuilder();
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        
+        StringBuilder sb=new StringBuilder(); 
+        
 
-    for(int i = 0; i < indexes.length; i++)//map the indexes 
-        map.put(indexes[i], i);
+        for(int i=0; i < s.length(); i++){
+            
+            boolean add=true; 
+            String res=""; 
+            
+            for (int j=0; j < indices.length; j++){
+                if (indices[j]==i && indices[j]+sources[j].length() <= s.length()){
+                    int sourceLength=sources[j].length();
+                     res=s.substring(indices[j], indices[j]+sourceLength);
+                    
+                        if(res.equals(sources[j])) {
+                            sb.append(targets[j]); 
+                            i+=sourceLength-1; 
+                            add=false; 
+                    }
+                }
+                
+            }
+            
+            if(add) sb.append(s.charAt(i)); 
 
-    for(int i = 0; i < s.length(); i++){
-        if(!map.containsKey(i)) {// i is not in indexes array
-            sb.append(s.charAt(i));//append current character 
-            continue;
+        
+            
         }
-
-        int idx = map.get(i);//get the index for sources and targets array
-
-        if(!s.substring(i, i + sources[idx].length()).equals(sources[idx])){//substring in s doesn't match string in source
-            sb.append(s.charAt(i));//add current character 
-            continue;
-        }
-
-        i += sources[idx].length() - 1;//move i to the end of matched substring
-        sb.append(targets[idx]);//append the corresponding target string 
+        
+        
+        return sb.toString(); 
     }
-    return sb.toString();
-}
 }
