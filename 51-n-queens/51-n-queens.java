f0c3aa36a7c36e29fcbe7ecc823bc1b6bdefa36d
Nesar Ahmed <nesar13@hotmail.com>
2022-07-08 19:22:19 -0400

    Time: 9 ms (39.33%), Space: 45.9 MB (36.14%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,62 +1,61 @@
 class Solution {
-    private int size;
-    private List<List<String>> solutions = new ArrayList<List<String>>();
     
+    int N; 
     public List<List<String>> solveNQueens(int n) {
-        size = n;
-        char emptyBoard[][] = new char[size][size];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                emptyBoard[i][j] = '.';
+        N=n; 
+        List<List<String>> res=new ArrayList<>(); 
+        char[][] board=new char[n][n]; 
+        
+        for (int i=0; i < n; i++){
+            for (int j=0; j < n; j++){
+                board[i][j]='.'; 
             }
         }
-
-        backtrack(0, new HashSet<>(), new HashSet<>(), new HashSet<>(), emptyBoard);
-        return solutions;
+        
+        backtrack(0, res,board,  new HashSet<>(), new HashSet<>(), new HashSet<>() ); 
+        return res; 
     }
     
-    // Making use of a helper function to get the
-    // solutions in the correct output format
-    private List<String> createBoard(char[][] state) {
-        List<String> board = new ArrayList<String>();
-        for (int row = 0; row < size; row++) {
-            String current_row = new String(state[row]);
-            board.add(current_row);
-        }
+    List<String> createBoard(char[][] board){
         
-        return board;
+        List<String> temp=new ArrayList<>(); 
+        for(int row=0; row<board[0].length; row++){
+            String currRow=new String(board[row]);
+            temp.add(currRow); 
+        }
+        return temp; 
     }
     
-    private void backtrack(int row, Set<Integer> diagonals, Set<Integer> antiDiagonals, Set<Integer> cols, char[][] state) {
-        // Base case - N queens have been placed
-        if (row == size) {
-            solutions.add(createBoard(state));
-            return;
+    
+    void backtrack(int row, List<List<String>> res, char[][] board, Set<Integer> cols, Set<Integer> diag, Set<Integer> antiDiag){
+        
+        if(row==N){
+            res.add(createBoard(board)); // return a list<String> of board
         }
         
-        for (int col = 0; col < size; col++) {
-            int currDiagonal = row - col;
-            int currAntiDiagonal = row + col;
-            // If the queen is not placeable
-            if (cols.contains(col) || diagonals.contains(currDiagonal) || antiDiagonals.contains(currAntiDiagonal)) {
-                continue;    
-            }
+        for (int col=0; col < N; col++){
             
-            // "Add" the queen to the board
-            cols.add(col);
-            diagonals.add(currDiagonal);
-            antiDiagonals.add(currAntiDiagonal);
-            state[row][col] = 'Q';
-
-            // Move on to the next row with the updated board state
-            backtrack(row + 1, diagonals, antiDiagonals, cols, state);
-
-            // "Remove" the queen from the board since we have already
-            // explored all valid paths using the above function call
-            cols.remove(col);
-            diagonals.remove(currDiagonal);
-            antiDiagonals.remove(currAntiDiagonal);
-            state[row][col] = '.';
+            int currDiag=row-col; 
+            int currAntiDiag=row+col; 
+            
+            if(cols.contains(col) || diag.contains(currDiag) || antiDiag.contains(currAntiDiag)) continue; 
+            
+            cols.add(col); 
+            diag.add(currDiag); 
+            antiDiag.add(currAntiDiag); 
+            board[row][col]='Q'; 
+            backtrack(row+1, res,board, cols, diag, antiDiag);
+            
+            cols.remove(col); 
+            diag.remove(currDiag);
+            antiDiag.remove(currAntiDiag); 
+            board[row][col]='.'; 
+            
         }
+        
+        return; 
+        
     }
+    
+    
 }
