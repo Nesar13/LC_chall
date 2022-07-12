77c7227b79bc2abbe94a6d3ae2ae3c97413155bb
Nesar Ahmed <nesar13@hotmail.com>
2022-07-11 20:08:30 -0400

    Time: 5 ms (21.62%), Space: 46 MB (54.80%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.java
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.java
@@ -18,36 +18,28 @@ class Node {
 */
 
 class Solution {
-    
-    
-  
     public List<Integer> postorder(Node root) {
-  
-        List<Integer> res=new ArrayList(); 
-        if (root==null) return res; 
         
         
-        ArrayDeque<Node> stack=new ArrayDeque(); 
-        
+       
+        List<Integer> res=new ArrayList<>();  
+        if(root==null) return res; 
+        ArrayDeque<Node> stack=new ArrayDeque<>();
         stack.push(root); 
         
         while (!stack.isEmpty()){
             
-            
-            Node curr=stack.pop();  
-            int N=curr.children.size();
-            res.add(curr.val);
-            for (int i =0; i < N ; i++){
-                
-                stack.push(curr.children.get(i)); 
+            Node curr=stack.pop(); 
+            res.add(curr.val); 
+            if(curr.children.size()==0) continue; 
+            for(Node child : curr.children){
+                stack.push(child); 
             }
+            
+            
         }
-        Collections.reverse(res);
-        return res ; 
         
+        Collections.reverse(res); 
+        return res;
     }
-    
-    
-   
-    
 }
