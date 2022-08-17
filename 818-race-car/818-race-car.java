e9b11398eade43509ac759733505a6debdd67fbc
Nesar Ahmed <nesar13@hotmail.com>
2022-08-16 20:46:41 -0400

    Time: 25 ms (70.74%), Space: 45.4 MB (67.41%) - LeetHub

--- 818-race-car/818-race-car.java
+++ 818-race-car/818-race-car.java
@@ -1,14 +1,22 @@
 class Solution {
     
     // target=3 
-    //  0    1   2   3
-    //p:0   1   3   7   2^(n-1) + sum(2^n-i)
-    //s:1   2   4   8
+    //    0   1   2   3
+    //pos:0   1   3   7   
+    //spe:1   2   4   8
     
+    // We can do a BFS solution if we realize that the positions are interconnected, if we reverse
+    // we just add that to the queue(position, speed, steps)
+    // We also have to store a visited array of the position and speed so we don't revisit the same
+    // numbers
+    // O(targetlog(target)) time | , could have visited every single position in both the directions i.e. from  
+    // target to +target so the total no of positions possible are O(target), but since we are reaching target 
+    // bounded by speed from 1,2,4,8... up to target, the total number of DISTINCT SPEEDS are O(log(target))
+    // Hence time complexity= Total distinct positions(target) * total distinct speeds (log(target)) = O(targetlog(target)).
     public int racecar(int target) {
         
         Deque<int[]> q=new ArrayDeque<>();
-        HashSet<String> visited = new HashSet<>(); // position, steps
+        HashSet<String> visited = new HashSet<>(); // position, speed
         q.offer(new int[]{0, 1, 0}); // pos, speed, steps
         
         while (!q.isEmpty()){
@@ -19,20 +27,14 @@ class Solution {
             
             if(pos==target) return steps; 
             if(visited.contains(pos + "," + speed)) continue; 
-            q.offer(new int[]{pos+speed, speed*2, steps+1});
+            q.offer(new int[]{pos+speed, speed*2, steps+1}); // must add new pos and speed
             
             if(pos+speed > target && speed >0 || pos+speed < target && speed < 0){
                 
                 speed= speed > 0? -1 : 1;
                 q.offer(new int[]{pos, speed, steps+1}); // must add the reversed, and counts as a step
             }
-             
-             
-            
-                              
-            
-            
-            
+
         }
         
         return -1; 
